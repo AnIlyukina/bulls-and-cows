1803226352138f3fa0d3db58c3fe8150f56337b8
AnnaIluykina <anyailyukina@yandex.ru>
2022-02-01 19:18:47 +0800

    Компонент Register начальная верстка

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <Register/>
+  </div>
+</template>
+
+<script>
+
+import Register from './components/Register.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Register
+  }
+}
+</script>
+
+<style>
+body{
+ margin: 0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #ffffff;
+  background-color: rgb(61, 129, 107);
+  
+  width: 100vw;
+  height: 100vh;
+  padding: 20px;
+  box-sizing: border-box;
+}
+</style>
